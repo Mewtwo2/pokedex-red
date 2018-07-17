900f09c8cfd4b1759d778e90e49136bbb7ef0452
Ibteda Sabur <akkount35@gmail.com>
2018-07-17 18:28:46 -0400

    added bunch of stuff

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function loadDoc(){
-  // console.log("Hello World");
+  console.log("Hello World");
 
   var xhhtp = new XMLHttpRequest();
 
   xhhtp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200) {
-      var parsedObject = JSON.parse(this.responseText)['stats'][1]['base_stat'];
+      var parsedObject = JSON.parse(this.responseText);
       console.log(parsedObject);
     }
   };
@@ -16,13 +16,8 @@ function loadDoc(){
   xhhtp.send();
 }
 
-
-// 200 - successful request
-// 503 - internal server error
-// 404 - file not found
-
-// Main HTTP Verbs
-// GET, POST PATCH, DELETE
-
-// POST - submitting info to a server (gmail sign up)
-// PATCH - changing something already on the server
+class Trainer {
+  constructor() {
+    this.skillset = {catchingSkill:5,trainingSkill:5,battlingSkill:5};
+  }
+}
