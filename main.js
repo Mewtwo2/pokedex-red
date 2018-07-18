4a6ac125cb8a9b3212409111e73a5cd5ea329a73
Ibteda Sabur <akkount35@gmail.com>
2018-07-17 22:56:00 -0400

    added some class code

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
-function loadDoc(){
-  console.log("Hello World");
+var hp;
+var atk;
+var def;
+// var abilities;
 
+function loadDoc() {
   var xhhtp = new XMLHttpRequest();
 
-  xhhtp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200) {
+  xhhtp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
       var parsedObject = JSON.parse(this.responseText);
       console.log(parsedObject);
     }
   };
 
-  var pikaPika = "pikachu"
+  var pikaPika = "pikachu";
 
-  xhhtp.open("GET",`https://pokeapi.co/api/v2/pokemon/${pikaPika}`,true);
-  // xhhtp.open("GET",`https://pokeapi-nycda.firebaseio.com/pokemon/25.json`,true);
+  xhhtp.open("GET", `https://pokeapi.co/api/v2/pokemon/${pikaPika}`, true);
+  // xhhtp.open("GET", `https://pokeapi-nycda.firebaseio.com/pokemon/25.json`, true); // This is the backup api call
   xhhtp.send();
 }
 
 class Trainer {
   constructor() {
-    this.skillset = {catchingSkill:5,trainingSkill:5,battlingSkill:5};
+    this.skillset = {
+      catchingSkill: 5,
+      trainingSkill: 5,
+      battlingSkill: 5
+    };
     this.pokemon = {};
+  }
+}
+
+class Red extends Trainer {
+  constructor() {
+    super();
+    this.skillset['catchingSkill'] = 100;
+    this.skillset['trainingSkill'] = 100;
+    this.skillset['battlingSkill'] = 100;
+
+    this.pokemon = {
+      pikachu: {name:"pikachu"},
+      mewtwo: {},
+      rapidash: {}
+    };
+  }
+
+  get(pokemon) {
+
   }
 }
