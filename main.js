f1fbdeef3127bfd1661dc66f51bcee126c3c00e4
Ibteda Sabur <akkount35@gmail.com>
2018-07-20 15:50:56 -0400

    added functionality where the pokedex data dissapears when you click another pokemon

--- main.js
+++ main.js
@@ -25,21 +25,24 @@ trainer = new Trainer('Red');
 function makePokemon(pkname) {
   clickedPokemon = pkname;
 
+  let pokedexInfoScreen = document.getElementById('pokedex-info');
+  pokedexInfoScreen.style.display = 'none';
+
   newPokemon = {
     abilities: []
   };
 
-  var xhttp = new XMLHttpRequest();
+  let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      var parsedObject = JSON.parse(this.responseText);
+      let parsedObject = JSON.parse(this.responseText);
       console.log(parsedObject);
       newPokemon.name = parsedObject['name'];
       newPokemon.hp = parsedObject['stats'][5]['base_stat'];
       newPokemon.atk = parsedObject['stats'][4]['base_stat'];
       newPokemon.def = parsedObject['stats'][3]['base_stat'];
 
-      var abilityArray = parsedObject['abilities'];
+      let abilityArray = parsedObject['abilities'];
       for (obj in abilityArray) {
         newPokemon.abilities.push(abilityArray[obj]['ability']['name']);
       }
@@ -65,10 +68,10 @@ function updatePokemon() {
 }
 
 function updateScreen(currentPokemon) {
-  var statList = document.getElementById('stat-list');
-  var listItems = document.getElementsByClassName('list-item');
-  var pokedexImage = document.getElementById('pokedex-image');
-  var pokedexInfoScreen = document.getElementById('pokedex-info');
+  let statList = document.getElementById('stat-list');
+  let listItems = document.getElementsByClassName('list-item');
+  let pokedexImage = document.getElementById('pokedex-image');
+  let pokedexInfoScreen = document.getElementById('pokedex-info');
 
   for (obj in allPokemon) {
     if (allPokemon[obj]['name'] == currentPokemon) {
