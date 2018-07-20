05172b61f3999e478961b1c8e870c83dce547779
Ibteda Sabur <akkount35@gmail.com>
2018-07-20 10:37:52 -0400

    added a button to generate stats because a race condition kept me from doing it by image click alone

--- main.js
+++ main.js
@@ -1,61 +1,65 @@
-Red = [];
+allPokemon = [];
+
+clickedPokemon = "";
 
 class Trainer {
   constructor(name) {
     this.name = name;
   }
 
-  get(name) {
-    var Pokemon = makePokemon(name);
-    console.log(Pokemon);
-    return Pokemon;
-  }
-
-  all(){
-    return Red;
+  all() {
+    return allPokemon;
   }
 }
 
-function makePokemon(iChooseYou) {
+trainer = new Trainer('Red');
 
-  for (each in Red) {
-    if (Red[each]['name'] == iChooseYou) {
-      return;
-    }
-  }
+function makePokemon(pkname) {
+  clickedPokemon = pkname;
 
-  let Poke = {
-
-  };
+  newPokemon = {};
 
   var xhttp = new XMLHttpRequest();
-
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var parsedObject = JSON.parse(this.responseText);
       console.log(parsedObject);
-      Poke.name = parsedObject['name'];
-      Poke.hp = parsedObject['stats'][5]['base_stat'];
-      Poke.atk = parsedObject['stats'][4]['base_stat'];
-      Poke.def = parsedObject['stats'][3]['base_stat'];
+      newPokemon.name = parsedObject['name'];
+      newPokemon.hp = parsedObject['stats'][5]['base_stat'];
+      newPokemon.atk = parsedObject['stats'][4]['base_stat'];
+      newPokemon.def = parsedObject['stats'][3]['base_stat'];
     }
   };
-
-  xhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${iChooseYou}`, true);
+  xhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${pkname}`, true);
   xhttp.send();
 
-  Red.push(Poke);
-console.log(Red);
-  updateScreen();
-
-  return Poke;
+  allPokemon.push(newPokemon);
+  console.log(allPokemon);
+  updatePokemon();
 }
 
+function updatePokemon() {
+  // console.log(allPokemon);
+  y = allPokemon.shift();
+  console.log(y.name);
+  allPokemon.push(y);
+  console.log(allPokemon[0].name);
+  // This is where Michaels code ends
+}
 
-function updateScreen(){
-  var statList = document.getElementById('ability-list');
+function updateScreen(currentPokemon) {
+  var statList = document.getElementById('stat-list');
   var listItems = document.getElementsByClassName('list-item');
-  z = Red[0]
-  console.log(z.name);
 
+  for (obj in allPokemon) {
+    if (allPokemon[obj]['name'] == currentPokemon) {
+      console.log(allPokemon[obj]['name']);
+    }
+  }
 }
+
+// click a pokemon image
+// loads API data
+// adds pokemon to allPokemon
+// create new button on screen that says 'Show stats for [x] pokemon'
+// use new button to then show stats on the screen
